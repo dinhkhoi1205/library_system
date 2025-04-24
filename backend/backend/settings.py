4bcbfb943126fa79890689ac2d11238d64c09071
dinhkhoi1205 <djjockey2004@gmail.com>
2025-04-25 00:53:52 +0700

    First commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,11 +94,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookdb',
-        'USER': 'root',
-        'PASSWORD': 'Admin@123',
-        'HOST': ''
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PWD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
     }
 }
 
