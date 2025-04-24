42ecd15ba58516a0f71b00bdba481f9082b127c0
dinhkhoi1205 <djjockey2004@gmail.com>
2025-04-24 21:52:02 +0700

    New3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,11 +95,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'NAME': 'bookdb',
+        'USER': 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': ''
     }
 }
 
